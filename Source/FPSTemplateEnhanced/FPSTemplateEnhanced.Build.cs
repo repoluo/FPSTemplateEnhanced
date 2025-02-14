e2e4ebe74041a85850b97d5dff10070bef21900e
repoluo <1909880919@qq.com>
2025-02-14 21:21:01 +0800

    FPS Template init and add CubActor

---
+++ Source/FPSTemplateEnhanced/FPSTemplateEnhanced.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FPSTemplateEnhanced : ModuleRules
+{
+	public FPSTemplateEnhanced(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
